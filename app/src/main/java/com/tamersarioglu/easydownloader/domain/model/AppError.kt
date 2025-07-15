03937d577ae46679fe00965864a676db71a8460a
Tamer Sarioglu <tamerthedark@hotmail.com>
2025-07-16 00:41:49 +0300

    refactor AppError to use data objects for error types

--- app/src/main/java/com/tamersarioglu/easydownloader/domain/model/AppError.kt
+++ app/src/main/java/com/tamersarioglu/easydownloader/domain/model/AppError.kt
@@ -4,9 +4,9 @@ package com.tamersarioglu.easydownloader.domain.model
  * Sealed class representing different types of errors that can occur in the app
  */
 sealed class AppError : Exception() {
-    object NetworkError : AppError()
-    object ServerError : AppError()
+    data object NetworkError : AppError()
+    data object ServerError : AppError()
     data class ApiError(val code: String, override val message: String) : AppError()
-    object UnauthorizedError : AppError()
+    data object UnauthorizedError : AppError()
     data class ValidationError(val field: String, override val message: String) : AppError()
 }
